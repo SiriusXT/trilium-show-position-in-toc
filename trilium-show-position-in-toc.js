a4b804550e56bbbd85c6688e223f7a1a58c6b275
SiriusXT <1160925501@qq.com>
2023-05-17 16:40:10 +0800

    0.2

--- trilium-show-position-in-toc.js
+++ trilium-show-position-in-toc.js
@@ -1,86 +1,80 @@
-/*
-trilium-show-position-in-toc
-https://github.com/SiriusXT/trilium-show-position-in-toc
-version:0.3
-*/
+// version: 0.2
 
+function checkIn(obj) {
+	var x = Number(window.event.clientX)
+	var y = Number(window.event.clientY)
 
-class ShowTocPosition extends api.NoteContextAwareWidget {
+	var div_x = Number(obj.getBoundingClientRect()
+		.left)
+	var div_x_width = Number(
+		obj.getBoundingClientRect()
+		.left + obj.clientWidth
+	)
 
-    get position() {
-        return 100;
-    }
-    get parentWidget() {
-        return 'center-pane';
-    }
-    doRender() {
-        this.$widget = $(`<style type="text/css">
-	.component.scrolling-container .note-detail-editable-text-editor.ck.ck-content{
-    overflow: visible;
-    }
-</style>`);
-        return this.$widget;
-    }
+	var div_y = Number(obj.getBoundingClientRect()
+		.top)
+	var div_y_height = Number(
+		obj.getBoundingClientRect()
+		.top + obj.clientHeight
+	)
 
-
-    async refreshWithNote() {
-        await this.showTocPosition();
-    }
-    scrollForToc(event, scrollingContainer = $(this)) {
-        let headerIndex = -1;
-        scrollingContainer.find(':header:not(section.include-note :header):not(div.note-list-widget.component :header)').each(function () {
-            headerIndex++;
-            var distance = $(this).offset().top - scrollingContainer.offset().top;
-            if (distance >= -20) {
-                if (distance > scrollingContainer.height() / 2) {
-                    headerIndex--;
-                }
-                return false;
-            }
-        });
-
-        var li = document.querySelectorAll("div.toc-widget span.toc li");
-        for (var i = 0; i < li.length; i += 1) {
-            if (i != headerIndex) {
-                li[i].style.setProperty("color", '');
-            } else {
-                li[i].style.setProperty("color", '#C70039');
-                li[i].scrollIntoView({
-                    block: "center",
-                    behavior: "smooth"
-                })
-            }
-        }
-    }
-    showTocPosition() {
-        const noteContext = this.noteContext;
-        const scrollForToc = this.scrollForToc;
-        $(document).ready(async function () {
-            setTimeout(async function () {
-                const isReadOnly = await noteContext.isReadOnly();
-                if (isReadOnly) {
-                    const $noteContainer = await noteContext.getContentElement();
-                    const scrollingContainer = $noteContainer.closest('.component.scrolling-container'); if (scrollingContainer.length > 0) {
-                        scrollingContainer.off('scroll', scrollForToc);
-                        scrollingContainer.on('scroll', scrollForToc);
-                        scrollForToc(undefined, scrollingContainer);
-                    }
-                } else {
-                    const textEditor = await noteContext.getTextEditor();
-                    const $noteContainer = $(textEditor.editing.view.domRoots.values().next().value);
-                    const scrollingContainer = $noteContainer.closest('.component.scrolling-container');
-                    if (scrollingContainer.length > 0) {
-                        scrollingContainer.off('scroll', scrollForToc);
-                        scrollingContainer.on('scroll', scrollForToc);
-                        scrollForToc(undefined, scrollingContainer);
-                    }
-                }
-            }, 300);
-        })
-    }
-
+	if (x > div_x && x < div_x_width && y > div_y && y < div_y_height) {
+		return true
+	} else {
+		return false
+	}
 }
 
-module.exports = new ShowTocPosition();
+var scrollFunc = function(event) {
+	var detail = document.querySelectorAll("div#rest-pane.component div.component.note-split.type-text:not(.hidden-ext) div.component.scrolling-container div.note-detail.component ");
+	if (detail.length != 0 && checkIn(detail[0]) == false) {
+		return;
+	}
+
+	var heading = document.querySelectorAll('div#rest-pane.component div.component.note-split.type-text:not(.hidden-ext) div.component.scrolling-container div.note-detail.component div.ck-content h2, div#rest-pane.component div.component.note-split.type-text:not(.hidden-ext) div.component.scrolling-container div.note-detail.component div.ck-content h3, div#rest-pane.component div.component.note-split.type-text:not(.hidden-ext) div.component.scrolling-container div.note-detail.component div.ck-content h4, div#rest-pane.component div.component.note-split.type-text:not(.hidden-ext) div.component.scrolling-container div.note-detail.component div.ck-content h5, div#rest-pane.component div.component.note-split.type-text:not(.hidden-ext) div.component.scrolling-container div.note-detail.component div.ck-content h6');
+	//console.log("heading.length:",heading.length);
+	if (heading.length == 0) {
+		return
+	}
+	var mouseY = event.clientY;
+	var minh = 100000;
+	var minhi = 100000;
+	for (var i = 0; i < heading.length; i += 1) {
+		var objY = parseInt(mouseY) - parseInt(heading[i].getBoundingClientRect()
+			.top);
+		if (objY > 0 && objY < minh) {
+			minh = objY;
+			minhi = i;
+		}
+	}
+	var li = document.querySelectorAll("div.toc-widget span.toc li");
+	for (var i = 0; i < li.length; i += 1) {
+		if (i != minhi) {
+			li[i].style.setProperty("color", '');
+		} else {
+			li[i].style.setProperty("color", '#dc413f');
+			li[i].scrollIntoView({
+				block: "center",
+				behavior: "smooth"
+			})
+
+		}
+
+	}
 
 
+}
+$(document)
+	.ready(function() {
+		function showToc() {
+			var detail = document.querySelectorAll("div#rest-pane");
+			if (detail.length == 1) {
+				detail[0].addEventListener("mouseover", scrollFunc);
+				detail[0].addEventListener("mousewheel", scrollFunc);
+			} else {
+				console.log("detail.length Error");
+			}
+		}
+
+		setTimeout(showToc, 500);
+	});
